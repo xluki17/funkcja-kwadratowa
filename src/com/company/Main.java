95963402373a6d3869603f9640c19e637323da4d
xluki17 <lukasz.z.wawszczak@gmail.com>
2019-01-04 20:28:49 +0100

    Rozwiązanie problemu z wykorzystaniem programowania obiektowego.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,15 @@
 package com.company;
 
-import java.util.Scanner;
-
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        double a, b, c;
-        Scanner odczyt = new Scanner(System.in);
-        System.out.print("podaj a=");
-        a = odczyt.nextDouble();
-        if (a == 0) {
-            System.out.println("TO NIE JEST FUNKCJA KWADRATOWA!!!");
-        } else {
-            System.out.print("podaj b=");
-            b = odczyt.nextDouble();
-            System.out.print("podaj c=");
-            c = odczyt.nextDouble();
+    public static void main(String[] args) throws IOException {
+        Trojmian trojmian=new Trojmian();
 
-            double delta, x1, x2;
-            delta = b * b - 4 * a * c;
-            System.out.println(delta);
+        trojmian.czytaj_dane();
+        trojmian.przetworz_dane();
+        trojmian.wyswietl_wynik();
 
-            if (delta > 0) {
-                x1 = (-b - Math.sqrt(delta)) / 2 * a;
-                System.out.println("x1=" + x1);
-                x2 = (-b + Math.sqrt(delta)) / 2 * a;
-                System.out.println("x2=" + x2);
-            } else if (delta == 0) {
-                x1 = (-b) / 2 * a;
-                System.out.print("x=" + x1);
-            } else {
-                System.out.println("Nie ma rozwiązania!");
-            }
-
-        }
     }
 }
---
+++ src/com/company/Trojmian.java
@@ -0,0 +1,69 @@
+package com.company;/*
+metoda jest odpowiedzialna za wczytanie
+        danych do programu oraz obsłużenie sytuacji, kiedy a = 0.
+        Zmienne a, b i c to liczby rzeczywiste wprowadzane
+        z klawiatury.
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Trojmian {
+
+    double a, b, c, delta, x1, x2;
+
+    public void czytaj_dane() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Program oblicza pierwiastki rownania kwadratowego.");
+        System.out.println("Podaj a: ");
+        a = Double.parseDouble(br.readLine());
+
+        if (a == 0) {
+            System.out.println("podano nieprawidłową wartość!");
+            System.exit(1);
+        } else {
+            System.out.println("Podaj b: ");
+            b = Double.parseDouble(br.readLine());
+            System.out.println("Podaj c: ");
+            c = Double.parseDouble(br.readLine());
+        }
+    }
+
+    public void przetworz_dane() // deklaracja i definicja metody przetworz_dane()
+    {
+        delta = b * b - 4 * a * c;
+
+        if (delta > 0) {
+            x1 = (-b - Math.sqrt(delta)) / 2 * a;
+            //    System.out.println("x1=" + x1);
+            x2 = (-b + Math.sqrt(delta)) / 2 * a;
+            //   System.out.println("x2=" + x2);
+        } else if (delta == 0) {
+            x1 = (-b) / 2 * a;
+            //   System.out.print("x=" + x1);
+        } else {
+            // System.out.println("Nie ma rozwiązania!");
+        }
+    }
+
+    public void wyswietl_wynik() // deklaracja i opis metody wyswietl_wynik()
+    {
+        System.out.print("Pierwiastki równania kwadratowego o parametrach a= " + a + "; b= " + b + "; c= " + c);
+        System.out.print(" wynosi ");
+        if (delta > 0) {
+            System.out.print("x1= ");
+            System.out.printf("%2.2f", x1);
+            System.out.print("x2= ");
+            System.out.printf("%2.2f", x2);
+        } else if (delta==0) {
+            System.out.print("x1= ");
+            System.out.printf("%2.2f", x1);
+        } else {
+            System.out.println("Nie ma rozwiązania!");
+        }
+
+    }
+
+}
